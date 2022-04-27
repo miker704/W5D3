1c378ecf57b71f7b5d4dc90ba0d2ce935f77d704
Leo Chung <leo.chung97@gmail.com>
2022-04-27 13:32:14 -0400

    Question.author

--- AA_questions/import_db.sql
+++ AA_questions/import_db.sql
@@ -1,17 +1,15 @@
 PRAGMA foreign_keys=ON;
 
-
- 
-    DROP TABLE IF EXISTS question_follows ;
-    DROP TABLE IF EXISTS replies ;
-    DROP TABLE IF EXISTS question_likes ;
-    DROP TABLE IF EXISTS users ;
-    DROP TABLE IF EXISTS questions ;
+    DROP TABLE IF EXISTS question_follows;
+    DROP TABLE IF EXISTS replies;
+    DROP TABLE IF EXISTS question_likes;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS questions;
 
     CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
-    fname TEXT DEFAULT '',
-    lname TEXT DEFAULT ''
+      id INTEGER PRIMARY KEY,
+      fname TEXT DEFAULT '',
+      lname TEXT DEFAULT ''
     );
 
     CREATE TABLE questions (
@@ -51,23 +49,27 @@ PRAGMA foreign_keys=ON;
         FOREIGN KEY(user_id) REFERENCES users(id)
     );
 
-
     INSERT INTO 
     users(fname,lname) 
-    VALUES('Leo','Chung'),
+    VALUES
+    ('Leo','Chung'),
     ('Michael','Ramoutar');
 
     INSERT INTO 
-    questions(title,body,user_id)
-    VALUES('question?','why im i so tired in the morning?',1),
-    ('question2?','why is sql so boring?',2);
-
-
+      questions(title,body,user_id)
+    VALUES
+      ('Why am I alive?','Is it just to make money?', 1),
+      ('Whats for lunch?','Is anyone feeling Halal food?', 2),
+      ('What ELO are you in?', 'I do not play League', 1),
+      ('Where am I?','I have dementia', 1);
 
     INSERT INTO
-    replies(body,reply_id,question_id,user_id)
-    VALUES('idk',NULL,1,2),
-    ('idk',1,1,1);
+      replies(body, reply_id, question_id, user_id)
+    VALUES
+      ('Dank reply',NULL,1,2),
+      ('This reply is so much danker than the last one',1,1,1),
+      ('Bro, I am the dankest', 2, 1, 2),
+      ('Bro you are pretty dank', 3, 1, 1);
 
 
 
--- AA_questions/questions_database.rb
+++ AA_questions/questions_database.rb
@@ -1,120 +1,205 @@
 require 'sqlite3'
 require 'singleton'
 
-    class QuestionsDatabase < SQLite3::Database
-        
-        include Singleton
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+  def initialize
+    super("questions.db")
+    self.type_translation=true
+    self.results_as_hash=true
+  end
+end
 
-        def initialize
-            super("questions.db")
-            self.type_translation=true
-            self.results_as_hash=true
+class Users 
+  attr_accessor :id, :fname, :lname
+  
+  def self.find_by_id(id)        
+    user_id = QuestionsDatabase.instance.execute(<<-SQL,id)
+      SELECT * FROM users WHERE id = ?;
+    SQL
+    Users.new(user_id.first)
+  end
+  
+  def self.find_by_name(fname,lname)
+    user = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        fname = ? AND
+        lname = ?;
+    SQL
+    Users.new(user.first)
+  end
 
-        end
+  def authored_questions
+    author = Questions.find_by_author_id(self.id)
+    raise "You haven't asked anything yet!" if !author
+    return author
+  end
 
-
-    end
-
-
-    class Users 
-
-        def self.find_by_id(id)        
-            user_id= QuestionsDatabase.instance.execute(<<-SQL,id)
-            SELECT * FROM users WHERE id = ?;
-            SQL
-            Users.new(user_id)
-        end
-
-        def initialize(options)
-            @id=options['id']
-            @fname=options['fname']
-            @lname=options['lname']
-        end
-
-
-    end
-
-
-    class Questions
-
-        def self.find_by_id(id)
-            questions= QuestionsDatabase.instance.execute(<<-SQL,id)
-            SELECT * FROM questions WHERE id = ?;
-            SQL
-            Questions.new(questions)
-        end
-
-        def initialize(options)
-            @id=options['id']
-            @title=options['title']
-            @body=options['body']
-        end
-    end
-
-
-
-    class QuestionFollows
-
-        def self.find_by_id(id)
-            question_follow=QuestionsDatabase.instance.execute(<<-SQL,id)
-            SELECT * FROM question_follows WHERE id = ?;
-            SQL
-            QuestionFollows.new(question_follow)
-
-        end
-
-        def initialize(options)
-            @id = options['id']
-            @question_id=options['question_id']
-            @user_id=options['user_id']
-
-        end
-
-    end
-
-    class Replies
-
-        def self.find_by_id(id)
-            reply = QuestionsDatabase.intstance.execute(<<-SQL,id)
-            SELECT * FROM replies WHERE id = ?;
-            SQL
-            Replies.new(reply)
-        end
-
-        def initialize(options)
-            @id = options['id']
-            @body=options['body']
-            @reply_id=options['reply_id']
-            @question_id=options['question_id']
-            @user_id=options['user_id']
-        end
-    end
+  def authored_replies
+    author = Replies.find_by_user_id(self.id)
+    raise "You haven't replied to anything!" if !author
+    return author
+  end
+  
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+end
 
 
-    class QLikes
+class Questions
+  attr_accessor :id, :title, :body
 
-        def self.find_by_id(id)
-            ques_likes=QuestionsDatabase.instance.execute(<<-SQL,id)
-            SELECT * FROM question_likes WHERE id = ?;
-            SQL
-            QLikes.new(ques_likes)
-        end
+  def self.find_by_id(id)
+    questions = QuestionsDatabase.instance.execute(<<-SQL,id)
+      SELECT * FROM questions WHERE id = ?;
+    SQL
+    Questions.new(questions.first)
+  end
 
-        def initialize(options)
-            @id=options['id']
-            @question_id=options['question_id']
-            @user_id=options['user_id']
-        end
-    end
+  def self.find_by_question(title, body)
+    question = QuestionsDatabase.instance.execute(<<-SQL, title, body)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        title = ? AND
+        body = ?;
+    SQL
+    Questions.new(question.first)
+  end
 
-
-
-
-    SQLite3::Database.new("questions.db") do |db|
-        db.execute("SELECT * FROM users") do |row|
-            p row
-        end
-    end
-
-
+  def self.find_by_author_id(user_id)
+    author = Users.find_by_id(user_id)
+    raise "No user found" if !author
     
+    questions = QuestionsDatabase.instance.execute(<<-SQL, author.id)
+      SELECT * FROM questions WHERE user_id = ?
+    SQL
+    questions.map {|q| Questions.new(q)}
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+  end
+end
+
+class QuestionFollows
+  attr_accessor :id, :question_id, :user_id
+  
+  def self.find_by_id(id)
+    question_follow = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT * FROM question_follows WHERE id = ?;
+    SQL
+    QuestionFollows.new(question_follow.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+  end
+end
+
+class Replies
+  attr_accessor :id, :body, :reply_id, :question_id, :user_id
+  
+  def self.find_by_id(id)
+    reply = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT * FROM replies WHERE id = ?;
+    SQL
+    Replies.new(reply.first)
+  end
+
+  def self.find_by_body(body)
+    reply = QuestionsDatabase.instance.execute(<<-SQL, body)
+      SELECT * FROM replies WHERE body = ?;
+    SQL
+    Replies.new(reply.first)
+  end
+
+  def self.find_by_user_id(id)
+    user = Users.find_by_id(id)
+
+    reply = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT * FROM replies WHERE user_id = ?
+    SQL
+    reply.map {|comment| Replies.new(comment)}
+  end
+
+  def self.find_by_question_id(id)
+    question = Questions.find_by_id(id)
+
+    reply = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT * FROM replies WHERE question_id = ?
+    SQL
+    reply.map {|comment| Replies.new(comment)}
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @body = options['body']
+    @reply_id = options['reply_id']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+  end
+end
+
+
+class Likes
+  attr_accessor :id, :question_id, :user_id
+  
+  def self.find_by_id(id)
+    ques_likes = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT * FROM question_likes WHERE id = ?;
+    SQL
+    Likes.new(ques_likes.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+  end
+end
+
+
+# SQLite3::Database.new("questions.db") do |db|
+#     db.execute("SELECT * FROM users") do |row|
+#         p row
+#     end
+# end
+
+testuser = QuestionsDatabase.instance.execute(<<-SQL, 1)
+      SELECT * FROM users WHERE id = ?;
+    SQL
+Users.new(testuser.first)
+
+testq = QuestionsDatabase.instance.execute(<<-SQL, 1)
+      SELECT * FROM questions WHERE id = ?
+    SQL
+Questions.new(testq.first)
+
+
+p question = Questions.find_by_author_id(1)
+puts
+p Replies.find_by_user_id(1)
+puts
+p Replies.find_by_question_id(1)
+puts
+p leo = Users.find_by_name('Leo', 'Chung')
+puts
+p leo.authored_questions
+puts
+p leo.authored_replies
+puts
+p question.author
